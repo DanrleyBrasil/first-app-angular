9a509f0423f6565d6f8f19d1e48a42c98967ab8e
danrleybrasil <danrleybrasil.s@gmail.com>
2025-06-01 22:39:47 -0300

    WIP - 01/06/2025

---
+++ src/app/_components/navbar/navbar.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar implements OnInit{
+  ngOnInit(): void {
+    this.message();
+  }
+
+  message(){
+
+    console.log('in a function')
+  }
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./_components/navbar/navbar";
+import { CommonModule } from '@angular/common';
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'first-app';
+  exibeNavbar: boolean = true;
 }
