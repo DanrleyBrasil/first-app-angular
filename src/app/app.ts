0a8959fbf1fea30da83abe1fe1f3ef8c72b77ba8
DanrleyBrasil <danrleybrasil.s@gmail.com>
2025-06-08 11:15:41 -0300

    Liberar branch para trabalhar no pc de casa

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from "./pages/certificados/certificados";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado],
+  imports: [Navbar, CommonModule, BaseUi, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
