cd222fbce72d8ed6f25f317e3f2755b6666079bb
danrleybrasil <danrleybrasil.s@gmail.com>
2025-06-08 20:24:14 -0300

    WIP 08/06/2025

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,10 +6,11 @@ import { SecondaryButton } from "./_components/secondary-button/secondary-button
 import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 import { BaseUi } from "./_components/base-ui/base-ui";
 import { Certificados } from "./pages/certificados/certificados";
+import { CertificadoForms } from "./pages/certificado-forms/certificado-forms";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, BaseUi, Certificados],
+  imports: [Navbar, CommonModule, BaseUi, CertificadoForms],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/pages/certificado-forms/certificado-forms.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-certificado-forms',
+  imports: [PrimaryButton, SecondaryButton],
+  templateUrl: './certificado-forms.html',
+  styleUrl: './certificado-forms.css'
+})
+export class CertificadoForms {
+
+}
